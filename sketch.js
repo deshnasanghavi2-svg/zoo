3e73186d9c10c05d4ce6e32dbb812aefc9b57ed5
deshnasanghavi2-svg <deshnasanghavi2@gmail.com>
2025-09-16 16:21:26 +0530

    commit
    first time trying to push files.

---
+++ sketch.js
@@ -0,0 +1,28 @@
+class Animal {
+   // special function that takes incoming parameters
+  constructor(breed="Human" , name="Nobody" , language="silence") {
+     //personal properties
+   this.breed = breed;
+   this.name = name;
+   this.language = language;
+  }
+  
+  // personal function
+  talk()  {
+  print(this.language)
+  
+  
+  expressYourself() {}
+  function setup () {
+    talk(  {})
+    
+    
+    
+    expressYourself() {
+    // string conco
+    
+  
+      
+      
+      
+      
